8934bf505333d04fc329a63294d11f5c2b1cee37
Zitting Miika <a1704460@myy.haaga-helia.fi>
2019-01-22 11:32:52 +0200

    Add new files

--- tehtävät/src/viikko02/Osoitetarra.java
+++ tehtävät/src/viikko02/Osoitetarra.java
@@ -1,16 +1,24 @@
+package viikko02;
 import java.util.Scanner;
 
 public class Osoitetarra {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("Anna etu-ja sukunimi: ");
-        String etunimi = lukija.nextLine();
-        String ekaKirjain = etunimi.substring(0,1).toUpperCase();
-        String loppuNimi = etunimi.substring(1).toLowerCase();
-
+        System.out.println("Anna etu-ja sukunimi: ");   
+        String firstName = reader.next();
+        String lastName = reader.nextLine().trim();
+        
+        String firstNameLetter = firstName.substring(0,1).toUpperCase();
+        String restFirstname = firstName.substring(1).toLowerCase();
+        
+        String firstSurLetter = lastName.substring(0,1).toUpperCase();
+        String restSurName = lastName.substring(1).toLowerCase();
+        
+        System.out.println(firstNameLetter + restFirstname + " " + firstSurLetter + restSurName);
+        
         System.out.println("Anna katuosoite: ");
-        String osoite = reader.nextline();
+
         
         System.out.println("Anna postinumero ja postitoimipaikka: ");
 
